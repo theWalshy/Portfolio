e93bbdb1d31925096857768e2a8e153e1f5b7d43
theWalshy <107557533+theWalshy@users.noreply.github.com>
2022-06-15 09:01:21 -0400

    Add files via upload

---
+++ simple_anim.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import random
+
+fig, ax = plt.subplots()
+scat = ax.scatter([],[])
+scat.axes.axis([-5, 5, -5, 5])
+num_frames = 100
+
+def update(point):
+    array = scat.get_offsets()
+    array = np.append(array, point)
+    scat.set_offsets(array)
+    return scat
+
+def data_gen():
+    for _ in range(0,num_frames):
+        point = np.random.normal(0, 1, 2)
+        yield point
+
+ani = animation.FuncAnimation(fig, update, data_gen, interval=50, blit=False, save_count=num_frames)
+ani.save('myanimation.gif',writer='imagemagick',fps=30)
+plt.show()
